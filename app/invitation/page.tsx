b4db1ef9f59edaa42e645d47972854600274db45
sooob <dltntnqo@naver.com>
2024-09-12 17:08:29 +0900

    main

--- app/invitation/page.tsx
+++ app/invitation/page.tsx
@@ -1,12 +1,5 @@
 "use client";
 import React from "react";
-import Greeting from "./components/Greeting";
-import Intro from "./components/Intro";
-import Gallery from "./components/Gallery";
-import { Way } from "./components/Way";
-import Attend from "./components/Attend";
-import SoobAccount from "./components/SoobAccount";
-import JJooAccount from "./components/JJooAccount";
 
 export default function page() {
   return <div></div>;
